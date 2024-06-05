e38e6146dfb07400a625f0f4b79c49f34f2cc35c
ThinkWithPbody <51525460+ThinkWithPbody@users.noreply.github.com>
2024-06-05 13:15:46 -0700

    Update ArticleTitle.tsx copy from quartz

--- quartz/components/ArticleTitle.tsx
+++ quartz/components/ArticleTitle.tsx
@@ -1,3 +1,4 @@
+/* 
 import { FileTitleIcon,IconFolderOptions  } from "../plugins/components/FileIcons";
 import { classNames } from "../util/lang";
 import { QuartzComponentConstructor, QuartzComponentProps } from "./types";
@@ -30,3 +31,25 @@ export default ((opts?: Partial<IconFolderOptions>) => {
   `;
 	return ArticleTitle;
 }) satisfies QuartzComponentConstructor;
+
+*/
+
+import { QuartzComponent, QuartzComponentConstructor, QuartzComponentProps } from "./types"
+import { classNames } from "../util/lang"
+
+const ArticleTitle: QuartzComponent = ({ fileData, displayClass }: QuartzComponentProps) => {
+  const title = fileData.frontmatter?.title
+  if (title) {
+    return <h1 class={classNames(displayClass, "article-title")}>{title}</h1>
+  } else {
+    return null
+  }
+}
+
+ArticleTitle.css = `
+.article-title {
+  margin: 2rem 0 0 0;
+}
+`
+
+export default (() => ArticleTitle) satisfies QuartzComponentConstructor
