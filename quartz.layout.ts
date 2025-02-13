3f514cdb41a38a60611bee8de98ffb346a789aae
ThinkWithPbody <51525460+ThinkWithPbody@users.noreply.github.com>
2025-02-12 21:27:18 -0500

    explorer add

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -5,59 +5,76 @@ import { IconFolderOptions } from "./quartz/plugins/components/FileIcons";
 // components shared across all pages
 
 const iconsOptions: IconFolderOptions = {
-  rootIconFolder: "quartz/static/icons",
-  default: {
-    file: "file",
-  },
+    rootIconFolder: "quartz/static/icons",
+    default: {
+        file: "file",
+    },
 };
 
 export const sharedPageComponents: SharedLayout = {
-  head: Component.Head(),
-  header: [],
-  afterBody: [],
-  footer: Component.Footer({
-    links: {
+    head: Component.Head(),
+    header: [],
+    afterBody: [],
+    footer: Component.Footer({
+        links: {
             "Email": "mailto:yifuding.twp@gmail.com",
             "LinkedIn": "https://www.linkedin.com/in/yifu-ding/",
             "Instagram": "https://www.instagram.com/yifu_ding_/",
-    },
-  }),
+        },
+    }),
 }
 
 // components for pages that display a single page (e.g. a single note)
 export const defaultContentPageLayout: PageLayout = {
-  beforeBody: [
-    Component.Breadcrumbs(),
-    Component.ArticleTitle(iconsOptions),
-    Component.ContentMeta({ showReadingTime: false }),
-    Component.TagList(),
-  ],
-  left: [
-    Component.PageTitle(),
-    Component.MobileOnly(Component.Spacer()),
-    Component.Search(),
-    Component.Darkmode(),
-    Component.DesktopOnly(Component.Explorer()),
-  ],
-  right: [
-    Component.Graph(),
-    Component.DesktopOnly(Component.TableOfContents()),
-    Component.Backlinks(),
-  ],
+    beforeBody: [
+        Component.Breadcrumbs(),
+        Component.ArticleTitle(iconsOptions),
+        Component.ContentMeta({ showReadingTime: false }),
+        Component.TagList(),
+    ],
+    left: [
+        Component.PageTitle(),
+        Component.MobileOnly(Component.Spacer()),
+        Component.Search(),
+        Component.Darkmode(),
+        Component.DesktopOnly(Component.Explorer()),
+    ],
+    right: [
+        Component.Graph(),
+        Component.DesktopOnly(Component.TableOfContents()),
+        Component.Backlinks(),
+    ],
 }
 
 // components for pages that display lists of pages  (e.g. tags or folders)
 export const defaultListPageLayout: PageLayout = {
     beforeBody: [Component.Breadcrumbs(), Component.ArticleTitle(), Component.ContentMeta()],
     left: [
-      Component.PageTitle(),
-      Component.MobileOnly(Component.Spacer()),
-      Component.Search(),
-      Component.Darkmode(),
-      Component.DesktopOnly(Component.Explorer()),
+        Component.PageTitle(),
+        Component.MobileOnly(Component.Spacer()),
+        Component.Search(),
+        Component.Darkmode(),
+        Component.DesktopOnly(Component.Explorer()),
     ],
     right: [],
 }
+
+// Explorer
+Component.Explorer({
+    title: "Explorer", // title of the explorer component
+    folderClickBehavior: "link", // what happens when you click a folder ("link" to navigate to folder page on click or "collapse" to collapse folder on click)
+    folderDefaultState: "open", // default state of folders ("collapsed" or "open")
+    useSavedState: true, // whether to use local storage to save "state" (which folders are opened) of explorer
+    // Sort order: folders first, then files. Sort folders and files alphabetically
+    sortFn: (a, b) => {
+      ... // default implementation shown later
+    },
+    filterFn: filterFn: (node) => node.name !== "tags", // filters out 'tags' folder
+    mapFn: undefined,
+    // what order to apply functions in
+    order: ["filter", "map", "sort"],
+  })
+
 
 // Graph
 Component.Graph({
