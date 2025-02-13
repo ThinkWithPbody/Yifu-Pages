de247dfba77da4f5e172b45ad18a6bec97981fff
ThinkWithPbody <51525460+ThinkWithPbody@users.noreply.github.com>
2025-02-13 19:10:04 +0000

    log file name too

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -35,7 +35,7 @@ export const defaultContentPageLayout: PageLayout = {
             filterFn: (node) => {
                 return (
                     node.file?.frontmatter?.tags?.includes("badtag") !== true &&
-                    (node.file?.frontmatter?.hide !== true || node.file?.frontmatter?.hide !== "true")
+                    node.file?.frontmatter?.hide !== true
                 )
             },
         }),
@@ -86,10 +86,10 @@ export const defaultListPageLayout: PageLayout = {
             folderClickBehavior: "link",
             folderDefaultState: "open",
             filterFn: (node) => {
-                console.log("HIDE VALUE:", node.file?.frontmatter?.hide, "TYPE:", typeof node.file?.frontmatter?.hide);
+                console.log("Processing:", node.file?.name, "HIDE VALUE:", node.file?.frontmatter?.hide, "TYPE:", typeof node.file?.frontmatter?.hide);
                 return (
                     node.file?.frontmatter?.tags?.includes("badtag") !== true &&
-                    (node.file?.frontmatter?.hide !== true || node.file?.frontmatter?.hide !== "true")
+                    node.file?.frontmatter?.hide !== true
                 )
             },
         }),
