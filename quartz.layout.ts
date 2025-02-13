1d4c3494434c7953fe73d3f1a6593cf125d4937e
ThinkWithPbody <51525460+ThinkWithPbody@users.noreply.github.com>
2025-02-13 19:14:13 +0000

    .name

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -86,7 +86,7 @@ export const defaultListPageLayout: PageLayout = {
             folderClickBehavior: "link",
             folderDefaultState: "open",
             filterFn: (node) => {
-                console.log("Processing:", node.file?.name, "HIDE VALUE:", node.file?.frontmatter?.hide, "TYPE:", typeof node.file?.frontmatter?.hide);
+                console.log("Processing:", node.name, node.displayName, "HIDE VALUE:", node.file?.frontmatter?.hide, "TYPE:", typeof node.file?.frontmatter?.hide);
                 return (
                     node.file?.frontmatter?.tags?.includes("badtag") !== true &&
                     node.file?.frontmatter?.hide !== true
